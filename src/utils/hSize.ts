ea5a6309bc743e8dc3acbac3f140a1d406dc74ef
Clansty <i@gao4.pw>
2022-04-03 14:02:26 +0800

    perf: MB -> MiB

--- src/utils/hSize.ts
+++ src/utils/hSize.ts
@@ -4,10 +4,10 @@ export default (size: number) => {
     if (size < BYTE)
         return size + 'B'
     if (size < Math.pow(BYTE, 2))
-        return (size / BYTE).toFixed(1) + 'KB'
+        return (size / BYTE).toFixed(1) + 'KiB'
     if (size < Math.pow(BYTE, 3))
-        return (size / Math.pow(BYTE, 2)).toFixed(1) + 'MB'
+        return (size / Math.pow(BYTE, 2)).toFixed(1) + 'MiB'
     if (size < Math.pow(BYTE, 4))
-        return (size / Math.pow(BYTE, 3)).toFixed(1) + 'GB'
-    return (size / Math.pow(BYTE, 4)).toFixed(1) + 'TB'
+        return (size / Math.pow(BYTE, 3)).toFixed(1) + 'GiB'
+    return (size / Math.pow(BYTE, 4)).toFixed(1) + 'TiB'
 }
